b87f3acee7ae81a093ed4e57db4fcbc835237ea0
mrprince <mrprince@qq.com>
2019-08-16 11:11:57 +0800

    spring boot + ouath2 + sso

---
+++ spring-boot-sso/app1/src/main/java/com/shekhargulati/app1/ApplicationSecurity.java
@@ -0,0 +1,22 @@
+package com.shekhargulati.app1;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+@Order(value = 0)
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated().and()
+                .logout().permitAll();
+    }
+}
